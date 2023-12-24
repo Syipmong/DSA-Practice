b158bb41bcb31e998e3edfebb8d4e951d1e51b02
syipmong <syipmong5@gmail.com>
2023-12-24 14:30:15 +0100

    bin in java

--- Binary.java
+++ Binary.java
@@ -2,10 +2,14 @@ public class Binary {
     public int list;
     public int value;
 
-    public Binary(int list, int value){
+    public void search(int list, int value){
         this.list = list;
         this.value = value;
 
+    }
+
+    public void verifyValue(int index){
+        
     }
 
 }
