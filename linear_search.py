05ca25824e0ffa317b2114250f775c71c2cb005f
syipmong <syipmong5@gmail.com>
2023-12-24 06:37:18 +0100

    linear search algorithm in python

--- linear_search.py
+++ linear_search.py
@@ -2,9 +2,9 @@ def linear_search(list, value):
     for i in range(0, len(list)):
         if list[i] == value:
             return i
-    return none
+    return None
 
 
 list = [1,2,3,4,5,6,7,8]
 
-print(linear_search(list, 5))
+print(linear_search(list, 10))
