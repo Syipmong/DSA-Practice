137e37d5672dbf4016f445a03ea194d00d192983
syipmong <syipmong5@gmail.com>
2023-12-24 06:41:39 +0100

    verifying our algol index

--- linear_search.py
+++ linear_search.py
@@ -4,7 +4,14 @@ def linear_search(list, value):
             return i
     return None
 
+def verify_search(index):
+    if index is not None:
+        print("index found at: ", index)
+    else:
+        print("Index not found")
+
 
 list = [1,2,3,4,5,6,7,8]
 
-print(linear_search(list, 10))
+result = linear_search(list, 10)
+print(verify_search(list))
