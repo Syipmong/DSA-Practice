ecbf8339ad9f401f6e7a2b9e8a810cba68fc6e4c
syipmong <syipmong5@gmail.com>
2023-12-26 00:43:16 +0100

    it worked

--- recursive_bst.py
+++ recursive_bst.py
@@ -1,4 +1,16 @@
 def recursive_binary(list, value):
     if len(list) == 0:
         return False
-    
+    else:
+        mid = (len(list))//2
+        if list[mid] == value:
+            return True
+        else:
+            if list[mid] < value:
+                return recursive_binary(list[mid+1:],value)
+            else:
+                return recursive_binary(list[:mid],value)
+    return False
+
+list = [1,2,3,4,5,6,7,8]
+print(recursive_binary(list, 1))
