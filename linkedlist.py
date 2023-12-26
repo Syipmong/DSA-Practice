7fc8e2af29cefd6be0bbbce96529479053a323dd
syipmong <syipmong5@gmail.com>
2023-12-26 14:13:33 +0100

    configuring the nodes

--- linkedlist.py
+++ linkedlist.py
@@ -1,3 +1,15 @@
 class Node:
     data = None
     next_node = None
+
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return f"Node: {self.data}"
+
+n1 = Node(10)
+print(n1)
+n1.next_node = 20
+print(f"Next node of n1 is :{n1.next_node}")
