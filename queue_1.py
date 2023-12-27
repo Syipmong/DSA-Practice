e534c2bde34b335fe56ac3e754e0dbd623256447
syipmong <syipmong5@gmail.com>
2023-12-27 20:31:13 +0100

    adding the enqueue and dequeue operations

--- queue_1.py
+++ queue_1.py
@@ -7,5 +7,8 @@ class Queue:
         """Add an element to the end of the queue"""
         self.items.append(item)
 
-    def dequeue():
+    def dequeue(self):
         """Remove and return the first item in the queue"""
+        if len(self.items) != 0:
+            self.items.pop(0)
+        return f"List is empty"
