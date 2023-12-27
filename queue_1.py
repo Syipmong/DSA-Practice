f3c65a76d47904f5fbb4132697cf8c80603c8d18
syipmong <syipmong5@gmail.com>
2023-12-27 20:36:40 +0100

    adding the size method

--- queue_1.py
+++ queue_1.py
@@ -6,9 +6,15 @@ class Queue:
     def enqueue(self,item):
         """Add an element to the end of the queue"""
         self.items.append(item)
+        print("added")
 
     def dequeue(self):
         """Remove and return the first item in the queue"""
         if len(self.items) != 0:
             self.items.pop(0)
         return f"List is empty"
+    
+    def size(self):
+        """Return the number of items in the queue"""
+
+   
