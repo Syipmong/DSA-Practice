b4cb325db92d0640eaae269db82836f8d579eaff
syipmong <syipmong5@gmail.com>
2023-12-28 21:02:35 +0100

    just testing

---
+++ recursion.cpp
@@ -0,0 +1,10 @@
+int main()
+{
+    // Declare variables of type integer and float.
+    int num1 = 5;
+    float num2 = 3.7f;
+    // Print the value of 'num1' to the console using printf function with format specifier %d.
+    std::cout<<"Hello";
+    // Print the value of 'num2' to the console using printf function with format specifier %.2f. This will display only two decimal
+    return 0
+}
