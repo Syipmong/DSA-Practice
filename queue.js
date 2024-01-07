7d848481d29c36fe78c1d4524792bb75f3baf876
syipmong <syipmong5@gmail.com>
2024-01-07 09:40:03 +0100

    implemented the queue search in javascript

--- queue.js
+++ queue.js
@@ -1,5 +1,41 @@
 class Queue{
+
     constructor(){
         this.items = [];
     }
+    add(item){
+        this.items.push(item);
+    }
+
+    size(){
+        return this.items.length;
+    }
+
+    isEmpty(){
+        return this.items.length == 0;
+    }
+
+    remove(){
+        if(this.isEmpty()){
+            return "Underflow";
+        }
+        return this.items.shift();
+
+    }
 }
+
+
+q = new Queue();
+
+q.add(1);
+q.add(2);
+q.add(3);
+q.add(4);
+console.log(q.size())
+q.remove()
+q.add(5);
+q.add(6);
+q.add(7);
+console.log(q.size())
+q.remove()
+console.log(q.size())
