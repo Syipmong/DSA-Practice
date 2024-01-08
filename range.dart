31c057f9cf9f1ce329a069f2b5603a368c9dc5a0
syipmong <syipmong5@gmail.com>
2024-01-08 12:39:02 +0100

    Add range iteration and create Person class

--- range.dart
+++ range.dart
@@ -4,6 +4,25 @@ void main(List<String> args) {
      for (var j = 0; j < 5; j++) {
        print(i);
      }
+
    }
+
+   //let's create our class instance
+    var person = Person('John', 30);
+
+    person.showOutput();
    
+}
+
+
+// Let's create a class
+
+class Person {
+  String name;
+  int age;
+  Person(this.name, this.age);
+  void showOutput() {
+    print(name);
+    print(age);
+  }
 }
--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-    
+def test_range(num):
+    while num <= 10:
+        print(num)
+        num += 1
+
+
+test_range(5)
