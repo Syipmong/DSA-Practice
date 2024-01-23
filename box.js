05f8d428f0700b84805757f61c3a513cf451d7e0
syipmong <syipmong5@gmail.com>
2024-01-24 00:10:10 +0100

    Add vertices and edges to Box class

--- box.js
+++ box.js
@@ -1,5 +1,21 @@
 class Box{
     constructor(){
-        
+        this.vertices = [];
+        this.edges = [];
+        this.faces = [];
+        this.l1 = null;
+        this.l2 = null;
+        this.l3 = null;
+        this.l4 = null;
+
     }
+
+    vertices(l1,l2,l3,l4){
+        this.l1 = l1;
+        this.l2 = l2;
+        this.l3 = l3;
+        this.l4 = l4;
+    }
+
+    
 }
