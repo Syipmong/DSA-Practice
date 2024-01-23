3e92bbd6cb97de974559f35846ac3f7d0698a47f
syipmong <syipmong5@gmail.com>
2024-01-23 23:41:46 +0100

    Remove unnecessary code and update console.log statement

--- linkedlist.js
+++ linkedlist.js
@@ -90,10 +90,10 @@ class LinkedList{
 const myLinkedList = new LinkedList();
 myLinkedList.append(10);
 myLinkedList.append(5);
-myLinkedList.append(16);
 myLinkedList.prepend(1);
 myLinkedList.insert(2, 99);
 myLinkedList.remove(2);
 myLinkedList.reverse();
 console.log(myLinkedList.printList());
 console.log(myLinkedList);
+
