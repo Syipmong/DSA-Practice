5afefc7a1f5dfb4f417ace81647decf6713eb21d
Yipmong Said <Syipmong5@gmail.com>
2024-03-02 01:51:36 +0100

    Update Fibonacci implementation and add recursive version

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,26 @@
 def fib(n):
     prev = 0
-    next = 0
+    next = 1
     if n == 1:
         return 1
     else:
-        prev = prev + next
+        new = prev + next
+        for i in range(2, n+1):
+            new = prev + next
+            prev = next
+            next = new
+        return new
+
+
+def fib_rec(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    else:
+        return fib_rec(n-1) + fib_rec(n-2)
+# Testing the functions
+print("Fibonacci number at position 10 is: ", fib(10))
+print("Fibonacci number at position 10 is: ", fib_rec(10))
+
         
